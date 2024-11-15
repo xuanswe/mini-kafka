82de422d672f0a85d589009bc96bc0b7ec5baee1
Xuan Nguyen <24372782+xuanswe@users.noreply.github.com>
2024-11-15 17:18:46 +0100

    Implement skeleton for server life cycle

---
+++ internal/encoders/request_encoder.go
@@ -0,0 +1,23 @@
+package encoders
+
+import (
+	"github.com/pkg/errors"
+	"github.com/xuanswe/mini-kafka/internal/models"
+	"github.com/xuanswe/mini-kafka/internal/support"
+	"io"
+	"strings"
+)
+
+func ReadRequest(r io.Reader) (*models.Request, error) {
+	if r == nil {
+		return nil, errors.Errorf("nil reader")
+	}
+
+	reader := support.EnsureBufferedReader(r)
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read line")
+	}
+
+	return &models.Request{Data: strings.TrimSpace(line)}, nil
+}
